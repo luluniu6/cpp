95a49f74d2a849da5738337f2c3c96addd854c0d
luluniu <2171442940@qq.com>
2024-05-07 22:31:22 +0800

    mystring基本完成

--- test_mystring.cpp
+++ test_mystring.cpp
@@ -26,7 +26,7 @@ namespace lala
 		// 	strcpy(_str, str);
 		// }
 		string(const char *str = "") // 构造函数
-									 // : _str(new char[strlen(str) + 1])
+			 // : _str(new char[strlen(str) + 1])
 		{
 			_size = strlen(str);
 			_capacity = _size;
@@ -73,6 +73,11 @@ namespace lala
 			assert(i < _size);
 			return _str[i];
 		}
+		char &operator[](size_t i)
+		{
+			assert(i < _size);
+			return _str[i];
+		}
 		void reserve(size_t n)
 		{
 			if (n > _capacity)
@@ -84,7 +89,6 @@ namespace lala
 				_capacity = n;
 			}
 		}
-
 		void push_back(char ch)
 		{
 			if (_size == _capacity) // 增容
@@ -95,6 +99,27 @@ namespace lala
 			_str[_size] = ch;
 			++_size;
 			_str[_size] = '\0';
+		}
+		void resize(size_t n, char ch = '\0')
+		{
+			if (n < _size)
+			{
+				_str[n] = '\0';
+				_size = n;
+			}
+			else
+			{
+				if (n > _capacity)
+				{
+					reserve(n);
+				}
+				for (size_t i = _size; i < n; i++)
+				{
+					_str[i] = ch;
+				}
+				_size = n;
+				_str[_size] = '\0';
+			}
 		}
 		void append(const char *str)
 		{
@@ -116,66 +141,166 @@ namespace lala
 			this->append(str);
 			return *this;
 		}
-		void insert(size_t pos, char ch)
+		string &insert(size_t pos, char ch)
 		{
-			assert(pos<_szie);
-			if(_size==_capacity)
+			assert(pos < _size);
+			if (_size == _capacity)
 			{
 				size_t newcapacity = _capacity == 0 ? 2 : _capacity * 2;
 				reserve(newcapacity);
 			}
-			int end=_size();
-			while(end>=pos)
+			int end = _size;
+			while (end >= pos)
 			{
-				_str[end+1]=_str[end];
+				_str[end + 1] = _str[end];
 				--end;
 			}
-			_str[pos]=ch;
-			++_szie;
+			_str[pos] = ch;
+			++_size;
+			return *this;
 		}
-		void insert(size_t pos, const char *str);
-		void erase(size_t pos, size_t len = npos);
-		size_t find(char ch, size_t pos = 0);
-		size_t find(const char *str, size_t pos = 0);
-		bool operator<(const string &s);
-		bool operator==(const string &s);
-		bool operator<=(const string &s);
-		bool operator>(const string &s);
-		bool operator>=(const string &s);
-		bool operator!=(const string &s);
+		string &insert(size_t pos, const char *str)
+		{
+			assert(pos < _size);
+			size_t len = strlen(str);
+			if (_size + len > _capacity)
+			{
+				reserve(_size + len);
+			}
+			int end = _size;
+			while (end >= pos)
+			{
+				_str[end + len] = _str[end];
+				end--;
+			}
+
+			// for (int i = 0; i < len; i++)
+			// {
+			// 	_str[pos + i] = str[i];
+			// }
+			strncpy(_str + pos, str, len);
+
+			_size += len;
+			return *this;
+		}
+		string &erase(size_t pos, size_t len = npos)
+		{
+			assert(pos < _size);
+			if (len >= _size - pos)
+			{
+				_str[pos] = '\0';
+				_size = pos;
+			}
+			else
+			{
+				size_t i = pos + len;
+				while (i <= _size)
+				{
+					_str[i - len] = _str[i];
+					++i;
+				}
+				_size -= len;
+			}
+			return *this;
+		}
+		size_t find(char ch, size_t pos = 0)
+		{
+			for (size_t i = pos; i < _size; i++)
+			{
+				if (_str[i] == ch)
+				{
+					return i;
+				}
+				return npos;
+			}
+		}
+		size_t find(const char *str, size_t pos = 0)
+		{
+			char *p = strstr(_str, str);
+			if (p == nullptr)
+			{
+				return npos;
+			}
+			else
+			{
+				return p - _str;
+			}
+		}
+		bool operator<(const string &s)
+		{
+			int ret = strcmp(_str, s._str);
+			return ret < 0;
+		}
+		bool operator==(const string &s)
+		{
+			int ret = strcmp(_str, s._str);
+			return ret == 0;
+		}
+		bool operator<=(const string &s)
+		{
+			return *this < s || *this == s;
+		}
+		bool operator>(const string &s)
+		{
+			return !(*this < s);
+		}
+		bool operator>=(const string &s)
+		{
+			return !(*this <= s);
+		}
+		bool operator!=(const string &s)
+		{
+			return !(*this == s);
+		}
 
 	private:
 		char *_str;
 		size_t _size;	  // 已经有多少个有效字符
 		size_t _capacity; // 能存多少个有效字符,\0不是有效字符
-		static size_t npos;
+
+		const static size_t npos = -1; // 64位2^64-1=18446744073709551615输出为-1
 	};
 
-	string ::npos = -1;
-
+	istream &operator>>(istream &in, string &s)
+	{
+		while (1)
+		{
+			char ch;
+			ch = in.get();
+			if (ch == ' ' || ch == '\n')
+			{
+				break;
+			}
+			else
+			{
+				s += ch;
+			}
+		}
+		return in;
+	}
 	ostream &operator<<(ostream &out, const string &s)
 	{
 		for (size_t i = 0; i < s.size(); i++)
 		{
-			cout << s[i];
+			out << s[i];
 		}
 		return out;
 	}
-	// void test_string1()
-	// {
-	// 	string s1("hello");
-	// 	string s2;
-	// 	for (size_t i = 0; i < s1.size(); i++)
-	// 	{
-	// 		s1[i] += 'a'
-	// 		cout << s1[i] << " ";
-	// 	}
-	// 	for (size_t i = 0; i < s2.size(); i++)
-	// 	{
-	// 		s2[i] += 'a';
-	// 		cout << s2[i] << " ";
-	// 	}
-	// }
+	void test_string1()
+	{
+		string s1("hello");
+		string s2("a");
+		for (size_t i = 0; i < s1.size(); i++)
+		{
+			s1[i] += 1;
+			cout << s1[i] << " ";
+		}
+		for (size_t i = 0; i < s2.size(); i++)
+		{
+			s2[i] += 1;
+			cout << s2[i] << " ";
+		}
+	}
 	void test_string2()
 	{
 		string s1("hello");
@@ -252,6 +377,50 @@ namespace lala
 		s1 += "hello";
 		cout << s1;
 	}
+	void test_string7()
+	{
+		string s1("helo");
+		s1.insert(2, 'l');
+		cout << s1 << endl;
+		s1.insert(3, "la");
+		cout << s1 << endl;
+
+		string s2("hello");
+		s2.reserve(10);
+		cout << s2 << endl;
+		cout << s2.size() << endl;
+		cout << s2.capacity() << endl;
+
+		s2.resize(8, 'x');
+		cout << s2 << endl;
+		cout << s2.size() << endl;
+		cout << s2.capacity() << endl;
+
+		s2.resize(18, 'a');
+		cout << s2 << endl;
+		cout << s2.size() << endl;
+		cout << s2.capacity() << endl;
+
+		s2.resize(2, 'a');
+		cout << s2 << endl;
+		cout << s2.size() << endl;
+		cout << s2.capacity() << endl;
+
+		s1.erase(3, 1);
+		cout << s1 << endl;
+	}
+	void test_string8()
+	{
+		string s1("abcdabcdef");
+		cout << s1.find("cde") << endl;
+		cout << s1.find("cdex") << endl;
+	}
+	void test_string9()
+	{
+		string s;
+		cin >> s;
+		cout << s;
+	}
 }
 int main()
 {
@@ -260,6 +429,57 @@ int main()
 	// lala::test_string3();
 	// lala::test_string4();
 	// lala::test_string5();
-	lala::test_string6();
+	// lala::test_string6();
+	// lala::test_string7();
+	// lala::test_string8();
+	lala::test_string9();
+
 	return 0;
 }
+
+// namespace lala_easy
+// {
+// 	//面试限时实现一个String类，不可能具备std：string的功能
+// 	//但至少要求能正确管理资源-->构造+析构+拷贝+operator=
+// 	class Sting
+// 	{
+// 	public:
+// 		string(const char* str="")
+// 			:_str(new char[strlen(str)+1])
+// 		{
+// 			strcpy(_str,str);
+// 		}
+// 		string(const string& s)//深拷贝-现代写法
+// 			:_str(nullptr)
+// 		{
+// 			string tmp(s._str);
+// 			swap(_str,tmp._str);
+// 		}
+// 		string& operator=(const string& s)
+// 		{
+// 			if(this!=&s)
+// 			{
+// 				string tmp(s);
+// 				swap(_str,tmp._str);
+// 			}
+// 			return *this;
+// 		}
+// 		size_t size() const
+// 		{
+// 			return strlen(_str);
+// 		}
+// 		char &operator[](size_t i)
+// 		{
+// 			assert(i < _size);
+// 			return _str[i];
+// 		}
+// 		~string()
+// 		{
+// 			delete[] _str;
+// 			_str=nullptr;
+// 		}
+// 	private:
+// 		char* _str;
+
+// 	};
+// }
