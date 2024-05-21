e5a6d755ffb0766b5b1f0deef53eeccee9a4dd54
luluniu <2171442940@qq.com>
2024-05-21 22:03:19 +0800

    mylist_day2

--- mylist.cpp
+++ mylist.cpp
@@ -3,37 +3,157 @@ using namespace std;
 namespace lala
 {
     template <class T>
-    struct _list_node
+    struct __list_node
     {
-        _list_node<T> *_next;
-        _list_node<T> *_prev;
+        __list_node<T> *_next;
+        __list_node<T> *_prev;
         T _data;
-    };
 
+        __list_node(const T &x = T())
+            : _data(x), _next(nullptr), _prev(nullptr)
+        {
+        }
+    };
+    //__list_iterator<T,T&,T*>  -> iterator
+    //__list_iterator<T,const T&,const T*>  ->const_iterator
+    template <class T, class Ref, class Ptr>
+    struct __list_iterator
+    {
+        typedef __list_node<T> Node;
+        typedef __list_iterator<T, Ref, Ptr> Self;
+        Node *_node;
+        __list_iterator(Node *node)
+            : _node(node)
+        {
+        }
+        Ref operator*() // 解引用
+        {
+            return _node->_data;
+        }
+        Ptr operator->()
+        {
+            return &_node->_data;
+        }
+        Self &operator++() //++it
+        {
+            _node = _node->_next;
+            return *this;
+        }
+        Self operator++(int) // it++ //后置++运算符重载,注意需要加int来与前置++重载做区分，语法这样规定。 //为什么返回值不用引用，因为不可以返回局部变量
+        {
+            Self tmp(*this);
+            //_node = _node->_next;
+            ++(*this);
+            return tmp;
+        }
+        Self &operator--() //--it
+        {
+            _node = _node->_prev;
+            return *this;
+        }
+        Self operator--(int) // it--
+        {
+            Self tmp(*this);
+            //_node = _node->_prev;
+            --(*this);
+            return tmp;
+        }
+        //(it != l.end())
+        bool operator!=(const Self &it)
+        {
+            return _node != it._node;
+        }
+        bool operator==(const Self &it)
+        {
+            return _node == it._node;
+        }
+    };
     template <class T>
     class list
     {
-        typedef _list_node<T> Node;
+        typedef __list_node<T> Node;
 
     public:
-    //带头双向循环链表
+        typedef __list_iterator<T, T&, T*> iterator;
+        typedef __list_iterator<T, const T&, const T*> const_iterator;
+
+        iterator begin()
+        {
+            return iterator(_head->_next);
+        }
+        iterator end()
+        {
+            return iterator(_head);
+        }
+        const_iterator begin() const
+        {
+            return const_iterator(_head->_next);
+        }
+        const_iterator end() const
+        {
+            return const_iterator(_head);
+        }
+        // 带头双向循环链表
         list()
         {
-            _head=new Node;
-            _head->next=_head;
-            _head->prev=_head;
+            _head = new Node;
+            _head->_next = _head;
+            _head->_prev = _head;
         }
-        void push_back(const T& x)
+        void push_back(const T &x)
         {
-            Node* tail=head->_prev;
-            Node* newnode=new Node(x);
-            tail->_next=newnode;
-            newnode->_prev=tail;
-            newnode->_next=head;
-            _head->_prev=newnode;
+            Node *tail = _head->_prev;
+            Node *newnode = new Node(x);
+            tail->_next = newnode;
+            newnode->_prev = tail;
+            newnode->_next = _head;
+            _head->_prev = newnode;
+            // 结构设计的优势，有没有节点插入都一样
         }
 
     private:
-    node* _head;
+        Node *_head;
     };
+
+    void print_list(const list<int>& lt)
+    {
+        list<int>::const_iterator it =lt.begin();
+        while(it!=lt.end())
+        {
+            cout<<*it<<" ";
+            it++;
+        }
+        cout<<endl;
+    }
+    void test_list1()
+    {
+        list<int> lt;
+        lt.push_back(1);
+        lt.push_back(2);
+        lt.push_back(3);
+        lt.push_back(4);
+
+        list<int>::iterator it = lt.begin();
+        while (it != lt.end())
+        {
+            cout << *it << " ";
+            ++it;
+        }
+        cout << endl;
+    }
+    void test_list2()
+    {
+        list<int> lt;
+        lt.push_back(1);
+        lt.push_back(2);
+        lt.push_back(3);
+        lt.push_back(4);
+        print_list(lt);
+    }
+}
+int main()
+{
+    //lala::test_list1();
+    lala::test_list2();
+    return 0;
 }
