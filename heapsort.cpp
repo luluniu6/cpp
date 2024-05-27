69e637cbdd3983458984f592d767c5837a2667b4
luluniu <2171442940@qq.com>
2024-05-27 22:51:25 +0800

    leetcode215. 数组中的第K个最大元素

--- heapsort.cpp
+++ heapsort.cpp
@@ -36,12 +36,12 @@ void heapify(int *a, int n, int i)
 void HeapSort(int *a, int n)
 {
 	int i;
-	//建堆--使用向下调整算法
+	// 建堆--使用向下调整算法
 	for (i = (n - 1 - 1) / 2; i >= 0; i--)
 	{
 		heapify(a, n, i);
 	}
-	//排序
+	// 排序
 	for (i = n - 1; i >= 0; i--)
 	{
 		swap(a[i], a[0]);
@@ -60,3 +60,29 @@ int main(int argc, char const *argv[])
 	return 0;
 }
 
+// https://leetcode.cn/problems/kth-largest-element-in-an-array/description/ 215. 数组中的第K个最大元素
+class Solution
+{
+public:
+	int findKthLargest(vector<int> &nums, int k)
+	{
+		// 建立k个数的小堆
+		// 时间复杂度O(N*logK)
+		// 空间复杂度O(K)
+		priority_queue<int, vector<int>, greater<int>> minheap;
+		size_t i = 0;
+		for (; i < k; i++)
+		{
+			minheap.push(nums[i]);
+		}
+		for (; i < nums.size(); ++i)
+		{
+			if (nums[i] > minheap.top())
+			{
+				minheap.pop();
+				minheap.push(nums[i]);
+			}
+		}
+		return minheap.top();
+	}
+};
