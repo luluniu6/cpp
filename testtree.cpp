f1cbcb27c228c337a1f70ef1c7a17df9fbd56d5d
luluniu <2171442940@qq.com>
2024-06-17 19:23:08 +0800

    二叉树遍历非递归补充写法

--- testtree.cpp
+++ testtree.cpp
@@ -70,6 +70,9 @@ public:
 };
 
 //https://leetcode.cn/problems/binary-tree-postorder-traversal/description/ 145. 二叉树的后序遍历
+//1与2是利用前序遍历中左右的顺序，将代码左右互换，成为中右左，然后反转数组为左右中实现
+//3与4基本一模一样
+//1.
 class Solution {
 public:
     vector<int> postorderTraversal(TreeNode* root) {
@@ -86,6 +89,95 @@ public:
             if(node->right) st.push(node->right);
         }
         reverse(ret.begin(),ret.end());
+        return ret;
+    }
+};
+//2.
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        stack<TreeNode*>st;
+        TreeNode* cur=root;
+        while(cur||!st.empty())
+        {
+            if(cur)
+            {
+                st.push(cur);
+                ret.push_back(cur->val);
+                cur=cur->right;
+            }
+            else
+            {
+                cur=st.top();
+                st.pop();
+                cur=cur->left;
+            }
+        }
+        reverse(ret.begin(),ret.end());
+        return ret;
+    }
+};
+//3.
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        TreeNode* lastnode = nullptr;
+        while (cur || !st.empty()) 
+        {
+            while (cur) 
+            {
+                st.push(cur);
+                cur = cur->left;
+            }
+            TreeNode* top = st.top();
+            if (top->right == nullptr || lastnode == top->right) 
+            {
+                ret.push_back(top->val);
+                lastnode = top;
+                st.pop();
+            } 
+            else 
+            {
+                cur = top->right;
+            }
+        }
+        return ret;
+    }
+};
+//4.
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int> ret;
+        stack<TreeNode*> st;
+        TreeNode* cur = root;
+        TreeNode* lastnode = nullptr;
+        while (cur || !st.empty()) 
+        {
+            if (cur) 
+            {
+                st.push(cur);
+                cur = cur->left;
+            } 
+            else 
+            {
+                TreeNode* top = st.top();
+                if (top->right == nullptr || lastnode == top->right) 
+                {
+                    ret.push_back(top->val);
+                    lastnode = top;
+                    st.pop();
+                } 
+                else 
+                {
+                    cur = top->right;
+                }
+            }
+        }
         return ret;
     }
 };
