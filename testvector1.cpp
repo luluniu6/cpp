02f3d44f6ebb0379b0e0bc4de2dc90c398de6962
luluniu6 <145818851+luluniu6@users.noreply.github.com>
2024-09-01 22:06:19 +0800

    增加vector的pair排序怎么写

--- testvector1.cpp
+++ testvector1.cpp
@@ -28,3 +28,33 @@ public:
         return vv;
     }
 };
+
+static bool cmp(const pair<int, int> &a, const pair<int, int> &b)
+{
+    // 以pair对的第2个数的大小从大往小排序
+    return a.second > b.second;
+}
+
+int main()
+{
+    vector<pair<int, int>> scores;
+    scores.push_back(make_pair(1, 3));
+    scores.push_back(make_pair(2, 1));
+    scores.push_back(make_pair(3, 2));
+    cout << "before sort : " << endl;
+    for (int i = 0; i < scores.size(); i++)
+    {
+        cout << scores[i].first << " " << scores[i].second << endl;
+    }
+    // sort(scores.begin(), scores.end(), cmp); // 向算法传递函数
+    sort(scores.begin(), scores.end(), [](const pair<int, int> &a, const pair<int, int> &b)
+         { return a.second > b.second; }); // lambda表达式
+    cout << "after sort : " << endl;
+    for (int i = 0; i < scores.size(); i++)
+    {
+        cout << scores[i].first << " " << scores[i].second << endl;
+    }
+    return 0;
+}
+
+// 3273. 对 Bob 造成的最少伤害（https://leetcode.cn/problems/minimum-amount-of-damage-dealt-to-bob/description/） 贪心算法邻项交换法
