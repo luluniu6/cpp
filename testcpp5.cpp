a497932945e9000b073d7cff0dd3cc7864eb151c
luluniu6 <145818851+luluniu6@users.noreply.github.com>
2024-09-23 22:26:00 +0800

    基本计算器（小数，空格，中缀转后缀，括号，加减乘除）

--- testcpp5.cpp
+++ testcpp5.cpp
@@ -4,6 +4,8 @@
 #include <stack>
 using namespace std;
 
+#include <bits/stdc++.h>
+
 // 将字符串转为十进制整数
 int stringtoint(string s)
 {
@@ -19,7 +21,9 @@ int stringtoint(string s)
     return num;
 }
 
-// 逆波兰表达式求值
+// 中缀转后缀之后就没有括号
+
+// 逆波兰表达式求值（后缀）
 int evalRPN(vector<string> &tokens)
 {
     stack<int> st;
@@ -52,7 +56,6 @@ int evalRPN(vector<string> &tokens)
     }
     return st.top();
 }
-
 int Priority(char c)
 {
     switch (c)
@@ -77,6 +80,11 @@ string InifToSuffix(string &s)
     for (int i = 0; i < s.size(); i++)
     {
         char ch = s[i];
+        if (ch == '-' && (i == 0 || !(s[i - 1] >= '0' && s[i - 1] <= '9')))
+        {
+            ret += ch;
+            continue;
+        }
         if (ch >= '0' && ch <= '9')
         {
             ret += ch;
@@ -96,11 +104,13 @@ string InifToSuffix(string &s)
         }
         else
         {
+
             while (!st.empty() && Priority(ch) <= Priority(st.top()))
             {
                 ret += st.top();
                 st.pop();
             }
+            st.push(ch);
         }
     }
     while (!st.empty())
@@ -110,42 +120,157 @@ string InifToSuffix(string &s)
     }
     return ret;
 }
+
 // int main()
 // {
-//     string s = "2*(3+5)+7/1-4";
+//     string s = "-2*(-3+5)+7/1-4";
 //     string ret = InifToSuffix(s);
 //     cout << ret;
 // }
 
 // 为了处理有括号的情况，可以利用递归
-int dfs(string s, int &i)
+// int dfs(string s, int &i)
+// { // 防止递归后回到'('的位置，所以传引用
+//     stack<int> st;
+//     char op = '+';
+//     int res = 0;
+//     int num = 0;
+//     for (i; i < s.size(); i++)
+//     {
+//         if (isdigit(s[i]))
+//             num = num * 10 + (s[i] - '0'); // 加()防止溢出
+//         if (s[i] == '(')
+//         {
+//             num = dfs(s, ++i); // 不能使用后置++因为cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
+//             i++;               // 递归出来从')'的下一位再开始
+//         }
+//         if ((!isdigit(s[i]) && s[i] != ' ') || (i == s.size() - 1))
+//         {
+//             int pre = 0;
+//             switch (op)
+//             {
+//             case '+':
+//                 st.push(num);
+//                 break;
+//             case '-':
+//                 st.push(-num);
+//                 break;
+//             case '*':
+//                 pre = st.top();
+//                 st.pop();
+//                 st.push(num * pre);
+//                 break;
+//             case '/':
+//                 pre = st.top();
+//                 st.pop();
+//                 st.push(pre / num);
+//                 break;
+//             }
+//             op = s[i];
+//             num = 0;
+//         }
+//         if (s[i] == ')')
+//             break;
+//     }
+//     while (!st.empty())
+//     {
+//         res += st.top();
+//         st.pop();
+//     }
+//     return res;
+// }
+
+// // 用栈实现基本计算器
+// int calculate(string s)
+// {
+//     int begin = 0;
+//     return dfs(s, begin);
+// }
+
+// 双指针删除空格函数
+string removebs(string &s)
+{
+    int i = 0, j = 0;
+    for (i, j; i < s.size(); i++)
+    {
+        if (s[i] != ' ')
+        {
+            s[j] = s[i];
+            j++;
+        }            
+    }
+    s.resize(j);
+    return s;
+}
+// int main()
+// {
+//     string s = "haha la   ";
+//     cout << removebs(s);
+//     return 0;
+// }
+
+// 为了处理有括号的情况，可以利用递归
+double dfs(string s, int &i)
 { // 防止递归后回到'('的位置，所以传引用
-    stack<int> st;
+    s.erase(remove(s.begin(), s.end(), ' '), s.end());
+    stack<double> st;
     char op = '+';
-    int res = 0;
-    int num = 0;
+    double res = 0;
+    double num = 0;
+    int begin = 0;
+    int point = 0;
     for (i; i < s.size(); i++)
     {
-        if (isdigit(s[i])) num = num * 10 + (s[i] - '0'); // 加()防止溢出
+        while (isdigit(s[i]) || s[i] == '.')
+        {
+            if (isdigit(s[i]))
+            {
+                if (point != 0 && i - begin > point)
+                    num = num + 0.1 * (s[i] - '0');
+                else
+                    num = num * 10 + (s[i] - '0'); // 加()防止溢出
+            }
+            if (s[i] == '.')
+            {
+                point = i - begin;
+            }
+            if (s[i] == ' ')
+                i++;
+            i++;
+        }
         if (s[i] == '(')
         {
-            num = dfs(s, ++i);//不能使用后置++因为cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
-            i++; // 递归出来从')'的下一位再开始
+            num = dfs(s, ++i); // 不能使用后置++因为cannot bind non-const lvalue reference of type 'int&' to an rvalue of type 'int'
+            i++;               // 递归出来从')'的下一位再开始
         }
-        if ((!isdigit(s[i]) && s[i] != ' ') || (i == s.size() - 1))
+        if (!isdigit(s[i]) || i == s.size() - 1)
         {
-            int pre = 0;
+            double pre = 0;
             switch (op)
             {
-            case '+':st.push(num);break;
-            case '-':st.push(-num);break;
-            case '*':pre = st.top();st.pop();st.push(num * pre);break;
-            case '/':pre = st.top();st.pop();st.push(pre / num);break;
+            case '+':
+                st.push(num);
+                break;
+            case '-':
+                st.push(-num);
+                break;
+            case '*':
+                pre = st.top();
+                st.pop();
+                st.push(num * pre);
+                break;
+            case '/':
+                pre = st.top();
+                st.pop();
+                st.push(pre / num);
+                break;
             }
             op = s[i];
             num = 0;
+            begin = i;
         }
-        if (s[i] == ')') break;
+        if (s[i] == ')')
+            break;
     }
     while (!st.empty())
     {
@@ -154,17 +279,15 @@ int dfs(string s, int &i)
     }
     return res;
 }
-
 // 用栈实现基本计算器
-int calculate(string s)
+double calculate(string s)
 {
     int begin = 0;
     return dfs(s, begin);
 }
-
-int main()
-{
-    string s = "-2  +(   3*5)-4";
-    cout << calculate(s);
-    return 0;
-}
+// int main()
+// {
+//     string s = "-2.5+(3*5)-4";
+//     cout << calculate(s);
+//     return 0;
+// }
