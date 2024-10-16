84407286db90276a92a8a093707be94fa400d20f
luluniu6 <145818851+luluniu6@users.noreply.github.com>
2024-10-16 23:22:14 +0800

    更正kmp

--- kmp.cpp
+++ kmp.cpp
@@ -2,62 +2,62 @@
 #include <string>
 #include <vector>
 using namespace std;
-void getNext(int* next, const string& s) 
+void getNext(int *next, const string &s)
 {
-    //初始化
-    int j = 0;//表示前缀末尾，同时表示i（包括i）之前字符串最长相同前缀和
+    // 初始化
+    int j = 0; // 表示前缀末尾，同时表示i（包括i）之前字符串最长相同前缀和
     next[0] = 0;
-    for(int i=1;i<s.size();i++)
+    for (int i = 1; i < s.size(); i++)
     {
-        //前后缀不相同
-        while(j>0&&s[i]!=s[j])
+        // 前后缀不相同
+        while (j > 0 && s[i] != s[j])
         {
             j = next[j - 1];
         }
-        //前后缀相同
-        if(s[i]==s[j])
+        // 前后缀相同
+        if (s[i] == s[j])
         {
             j++;
         }
-        //更新next
+        // 更新next
         next[i] = j;
     }
 }
-int strStr(string haystack, string needle,int pos=0)
+int strStr(string haystack, string needle, int pos = 0)
 {
-    if(needle.size()==0) return 0;
-    vector<int> next(needle.size(),0);
-    getNext(&next[0],needle);
-    int j = pos;
-    for(int i=pos;i<haystack.size();i++)
+    if (needle.size() == 0)
+        return 0;
+    vector<int> next(needle.size(), 0);
+    getNext(&next[0], needle);
+    int j = 0;
+    for (int i = pos; i < haystack.size(); i++)
     {
-        while(j>pos&&haystack[i]!=needle[j])
+        while (j > pos && haystack[i] != needle[j])
         {
             j = next[j - 1];
         }
-        if(haystack[i]==needle[j]) j++;
-        if(j==needle.size())
+        if (haystack[i] == needle[j])
+            j++;
+        if (j == needle.size())
         {
-            if(i - needle.size()+1>=pos) return i - needle.size()+1;
-            else return -1;
-        }   
+            return i - needle.size() + 1;
+        }
     }
     return -1;
 }
 
 void test_kmp()
 {
-    string haystack,needle;
-    cin>>haystack;
-    cin>>needle;
-    cout<<strStr(haystack,needle,4);
+    string haystack, needle;
+    cin >> haystack;
+    cin >> needle;
+    cout << strStr(haystack, needle, 4);
 }
-int main()
-{
-    test_kmp();
-    return 0;
-}
-
+// int main()
+// {
+//     test_kmp();
+//     return 0;
+// }
 
 #include <iostream>
 #include <vector>
@@ -66,126 +66,159 @@ int main()
 using namespace std;
 
 // 三元组类，表示矩阵中的一个非零元素
-class Triple {
+class Triple
+{
 public:
     int row, col, val;
-    
+
     Triple(int r = 0, int c = 0, int v = 0) : row(r), col(c), val(v) {}
 
     // 排序用，按行列顺序排序
-    bool operator<(const Triple& t) const {
+    bool operator<(const Triple &t) const
+    {
         return row == t.row ? col < t.col : row < t.row;
     }
 };
 
 // 稀疏矩阵类
-class TSMatrix {
+class TSMatrix
+{
 private:
     int rows, cols;
-    vector<Triple> elements; 
+    vector<Triple> elements;
 
 public:
     TSMatrix(int m = 0, int n = 0) : rows(m), cols(n) {}
 
     // 输入稀疏矩阵
-    void inputMatrix(int len) {
-        for (int i = 0; i < len; ++i) {
+    void inputMatrix(int len)
+    {
+        for (int i = 0; i < len; ++i)
+        {
             int r, c, v;
             char tmp;
-            cin >> tmp >> r >> tmp >> c >> tmp >> v >> tmp;//"(r,c,v)"
+            cin >> tmp >> r >> tmp >> c >> tmp >> v >> tmp; //"(r,c,v)"
             elements.emplace_back(r, c, v);
         }
-        sort(elements.begin(), elements.end());  // 按行列顺序排序
+        sort(elements.begin(), elements.end()); // 按行列顺序排序
     }
 
-    int getRows() const {
+    int getRows() const
+    {
         return rows;
     }
 
-    int getCols() const {
+    int getCols() const
+    {
         return cols;
     }
 
-    int getNonZeroCount() const {
+    int getNonZeroCount() const
+    {
         return elements.size();
     }
 
-    const vector<Triple>& getElements() const {
+    const vector<Triple> &getElements() const
+    {
         return elements;
     }
 
     // 矩阵相加
-    TSMatrix add(const TSMatrix& B) const {
+    TSMatrix add(const TSMatrix &B) const
+    {
         TSMatrix result(rows, cols);
         int i = 0, j = 0;
-        while (i < elements.size() && j < B.getElements().size()) {
-            const Triple& a = elements[i];
-            const Triple& b = B.getElements()[j];
-            if (a.row == b.row && a.col == b.col) {
+        while (i < elements.size() && j < B.getElements().size())
+        {
+            const Triple &a = elements[i];
+            const Triple &b = B.getElements()[j];
+            if (a.row == b.row && a.col == b.col)
+            {
                 int sum = a.val + b.val;
-                if (sum != 0) result.elements.emplace_back(a.row, a.col, sum);
+                if (sum != 0)
+                    result.elements.emplace_back(a.row, a.col, sum);
                 i++;
                 j++;
-            } else if (a.row < b.row || (a.row == b.row && a.col < b.col)) {
+            }
+            else if (a.row < b.row || (a.row == b.row && a.col < b.col))
+            {
                 result.elements.push_back(a);
                 i++;
-            } else {
+            }
+            else
+            {
                 result.elements.push_back(b);
                 j++;
             }
         }
-        while (i < elements.size()) {
+        while (i < elements.size())
+        {
             result.elements.push_back(elements[i++]);
         }
-        while (j < B.getElements().size()) {
+        while (j < B.getElements().size())
+        {
             result.elements.push_back(B.getElements()[j++]);
         }
         return result;
     }
 
     // 矩阵相减
-    TSMatrix subtract(const TSMatrix& B) const {
+    TSMatrix subtract(const TSMatrix &B) const
+    {
         TSMatrix result(rows, cols);
         int i = 0, j = 0;
-        while (i < elements.size() && j < B.getElements().size()) {
-            const Triple& a = elements[i];
-            const Triple& b = B.getElements()[j];
-            if (a.row == b.row && a.col == b.col) {
+        while (i < elements.size() && j < B.getElements().size())
+        {
+            const Triple &a = elements[i];
+            const Triple &b = B.getElements()[j];
+            if (a.row == b.row && a.col == b.col)
+            {
                 int diff = a.val - b.val;
-                if (diff != 0) result.elements.emplace_back(a.row, a.col, diff);
+                if (diff != 0)
+                    result.elements.emplace_back(a.row, a.col, diff);
                 i++;
                 j++;
-            } else if (a.row < b.row || (a.row == b.row && a.col < b.col)) {
+            }
+            else if (a.row < b.row || (a.row == b.row && a.col < b.col))
+            {
                 result.elements.push_back(a);
                 i++;
-            } else {
+            }
+            else
+            {
                 result.elements.emplace_back(b.row, b.col, -b.val);
                 j++;
             }
         }
-        while (i < elements.size()) {
+        while (i < elements.size())
+        {
             result.elements.push_back(elements[i++]);
         }
-        while (j < B.getElements().size()) {
-            const Triple& b = B.getElements()[j++];
+        while (j < B.getElements().size())
+        {
+            const Triple &b = B.getElements()[j++];
             result.elements.emplace_back(b.row, b.col, -b.val);
         }
         return result;
     }
 
     // 输出矩阵
-    void print() const {
+    void print() const
+    {
         cout << rows << " " << cols << " " << elements.size() << endl;
-        if (!elements.empty()) {
-            for (const auto& elem : elements) {
+        if (!elements.empty())
+        {
+            for (const auto &elem : elements)
+            {
                 cout << "(" << elem.row << "," << elem.col << "," << elem.val << ")";
             }
-            //cout << endl;
+            // cout << endl;
         }
     }
 };
 
-// int main() {
+// int main()
+// {
 //     // 输入矩阵A
 //     int m, n, lenA;
 //     cin >> m >> n >> lenA;
@@ -198,13 +231,15 @@ public:
 //     TSMatrix B(m, n);
 //     B.inputMatrix(lenB);
 
-//     //A+B
+//     // A+B
 //     TSMatrix sum = A.add(B);
 //     sum.print();
 //     cout << endl;
-//     //A-B
+//     // A-B
 //     TSMatrix diff = A.subtract(B);
 //     diff.print();
+//     if (diff.getNonZeroCount() == 0)
+//         cout << endl;
 
 //     return 0;
 // }
@@ -242,7 +277,7 @@ public:
 //                 }
 //             }
 //         }
-       
+
 //     }
 //     if(ret.size() == 0) {
 //         cout << "NONE";
